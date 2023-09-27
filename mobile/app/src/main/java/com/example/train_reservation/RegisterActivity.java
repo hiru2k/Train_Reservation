14137639463316783d493e6723a03c828c06629c
hiru2k <mudannayakehiruni90@gmail.com>
2023-09-27 08:51:52 +0530

    Cretaed register page

--- mobile/app/src/main/java/com/example/train_reservation/RegisterActivity.java
+++ mobile/app/src/main/java/com/example/train_reservation/RegisterActivity.java
@@ -1,2 +1,41 @@
-package com.example.train_reservation;public class RegisterActivity {
+package com.example.train_reservation;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle registration logic here
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                // Call the registration function
+                registerUser(username, password);
+            }
+        });
+    }
+
+    private void registerUser(String username, String password) {
+        // Implement registration logic here
+        // You can use the code from the previous response for database operations
+    }
 }
