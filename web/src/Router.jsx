6ac789512c14c5d67e082f568d74244bcc59d0b7
hiru2k <mudannayakehiruni90@gmail.com>
2023-09-28 21:10:24 +0530

    set routes

--- web/src/Router.jsx
+++ web/src/Router.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom"; // Import BrowserRouter and Route from react-router-dom
 
 import Login from "./components/mainpages/auth/Login";
+import CreateUser from "./components/mainpages/admin/CreateUser";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />{" "}
-        {/* Use "element" instead of "component" */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/createUser" element={<CreateUser />} /> {}
       </Routes>
     </BrowserRouter>
   );
