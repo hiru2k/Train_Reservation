62bfbc576f3e4b397b90346d4c66fc028fce4c94
hiru2k <mudannayakehiruni90@gmail.com>
2023-09-29 08:06:42 +0530

    added new routes

--- web/src/Router.jsx
+++ web/src/Router.jsx
@@ -3,13 +3,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"; // Import Brows
 
 import Login from "./components/mainpages/auth/Login";
 import CreateUser from "./components/mainpages/admin/CreateUser";
+import UserProfile from "./components/mainpages/common/UserProfile";
+import CreateTravaler from "./components/mainpages/travelAgent/CreateTravaler";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/createUser" element={<CreateUser />} /> {}
+        <Route path="/createUser" element={<CreateUser />} />
+        <Route path="/userProfile" element={<UserProfile />} />
+        <Route path="/createTraveler" element={<CreateTravaler />} />
+
+        {}
       </Routes>
     </BrowserRouter>
   );
