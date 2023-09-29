b83a41bc0c91a03c465c0347b5f31fc4be41fee4
hiru2k <mudannayakehiruni90@gmail.com>
2023-09-29 19:51:01 +0530

    model rename

---
+++ backend/Models/TrainModel.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace backend.Models
+{
+    public class TrainModel
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("TrainNumber")]
+        public string? TrainNumber { get; set; }
+
+        [BsonElement("TrainName")]
+        public string? TrainName { get; set; }
+    }
+}
