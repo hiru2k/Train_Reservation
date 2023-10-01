f484c1e5c5a1fe01c1b2677e24dbcb81d6044863
hiru2k <mudannayakehiruni90@gmail.com>
2023-10-01 12:59:35 +0530

    updated default header

--- web/src/DefaultHeader.jsx
+++ web/src/DefaultHeader.jsx
@@ -1,11 +1,27 @@
 import axios from "axios";
 
-const axiosInstance = axios.create();
-
-// Set default headers for the axios instance
+const baseURL = import.meta.env.VITE_API_URL;
 const token = localStorage.getItem("token");
-if (token) {
-  axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-}
+
+const axiosInstance = axios.create({
+  baseURL: baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
