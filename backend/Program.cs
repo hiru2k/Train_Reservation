e7847413a7fb279716b1d2cc799d292ec809129c
hiru2k <mudannayakehiruni90@gmail.com>
2023-10-01 12:57:13 +0530

    configure jwt in backend

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,11 +1,15 @@
 using backend.Data;
 using backend.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using MongoDB.Driver;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // configs
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DB"));
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
 
 // Add services to the container.
 
@@ -17,11 +21,26 @@ builder.Services.AddCors((o) =>
 {
     o.AddPolicy("Allow FE", (conf) =>
     {
-        conf.WithOrigins("http://localhost:3000");
+        conf.WithOrigins("http://localhost:3000", "http://localhost:3000/");
         conf.AllowAnyHeader();
         conf.AllowAnyMethod();
     });
 });
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(o =>
+    {
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = true
+        };
+    });
 
 // MongoDB Configuration
 var connectionString = builder.Configuration.GetSection("DB:ConnectionString").Value;
@@ -49,6 +68,7 @@ app.UseHttpsRedirection();
 
 app.UseCors("Allow FE");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
