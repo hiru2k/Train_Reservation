5bee46c4c4c0bf65387e45be84b004d856b69680
hiru2k <mudannayakehiruni90@gmail.com>
2023-10-02 18:20:05 +0530

    created endUser api

---
+++ backend/Models/EndUserModel.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace backend.Models
+{
+    public class EndUserModel
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("Username")]
+        public string? Username { get; set; }
+
+        [BsonElement("NIC")]
+        public string? NIC { get; set; }
+        [BsonElement("Role")]
+        public string? Role { get; set; }
+        [BsonElement("Password")]
+        public string? Password { get; set; }
+        [BsonElement("Email")]
+
+        public string? Email { get; set; }
+        [BsonElement("Phone")]
+        public string? Phone { get; set; }
+        [BsonElement("Status")]
+        public string? Status { get; set; }
+
+
+    }
+}
