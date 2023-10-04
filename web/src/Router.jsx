98f2b0c563e204960747b1b81ba478a5e7d97b16
hiru2k <mudannayakehiruni90@gmail.com>
2023-10-04 16:16:48 +0530

    set role based navigation

--- web/src/Router.jsx
+++ web/src/Router.jsx
@@ -1,31 +1,42 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom"; // Import BrowserRouter and Route from react-router-dom
+import { useUserContext } from "./UserContext";
 
 import Login from "./components/mainpages/auth/Login";
 import CreateUser from "./components/mainpages/admin/CreateUser";
 import UserProfile from "./components/mainpages/common/UserProfile";
 import CreateTraveler from "./components/mainpages/travelAgent/CreateTraveler";
 import TravelerList from "./components/mainpages/common/TravelerList";
-import UpdateTraveler from "./components/mainpages/travelAgent/UpdateTraveler";
+
 import EndUserProfile from "./components/mainpages/common/EndUserProfile";
 import BackOfficerHome from "./components/mainpages/backOfficer/BackOfficerHome";
 import TravelAgentHome from "./components/mainpages/travelAgent/TravelAgentHome";
 
 function Router() {
+  const { role } = useUserContext();
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/createUser" element={<CreateUser />} />
-        <Route path="/userProfile" element={<UserProfile />} />
-        <Route path="/createTraveler" element={<CreateTraveler />} />
-        <Route path="/travelerList" element={<TravelerList />} />
-        <Route path="/updateTraveler" element={<UpdateTraveler />} />
-        <Route path="/endUserProfile/:nic" element={<EndUserProfile />} />
-        <Route path="/backOfficerHome" element={<BackOfficerHome />} />
-        <Route path="/travelAgentHome" element={<TravelAgentHome />} />
+        {role === "Admin" && (
+          <Route path="/createUser" element={<CreateUser />} />
+        )}
+        {role === "Back Officer" && (
+          <Route path="/backOfficerHome" element={<BackOfficerHome />} />
+        )}
+        {role === "Travel Agent" && (
+          <Route path="/travelAgentHome" element={<TravelAgentHome />} />
+        )}
 
-        {}
+        {(role === "Back Officer" || role === "Travel Agent") && (
+          <>
+            <Route path="/userProfile" element={<UserProfile />} />
+            <Route path="/createTraveler" element={<CreateTraveler />} />
+            <Route path="/travelerList" element={<TravelerList />} />
+
+            <Route path="/endUserProfile/:nic" element={<EndUserProfile />} />
+          </>
+        )}
       </Routes>
     </BrowserRouter>
   );
