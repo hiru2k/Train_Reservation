ac891d8461f31f34b6591658038242d8c74126c6
hiru2k <mudannayakehiruni90@gmail.com>
2023-10-07 17:41:09 +0530

    changed call type to LoginResponse

--- mobile/app/src/main/java/com/example/train_reservation/utils/LoginAPIInterface.java
+++ mobile/app/src/main/java/com/example/train_reservation/utils/LoginAPIInterface.java
@@ -1,12 +1,16 @@
 package com.example.train_reservation.utils;
 
+import com.example.train_reservation.models.User;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
+
+
 
 public interface LoginAPIInterface
 
 {
     @POST("login") // endpoint// endpoint for login
-    Call<Void> loginUser(@Body LoginRequestData loginUser);
+    Call<LoginResponse> loginUser(@Body User loginUser);
 }
