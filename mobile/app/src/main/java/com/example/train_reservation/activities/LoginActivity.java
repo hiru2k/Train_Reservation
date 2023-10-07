b93d30de8e07325324b0e67945161db306b6e47d
hiru2k <mudannayakehiruni90@gmail.com>
2023-10-07 07:35:16 +0530

    cerated login activity

---
+++ mobile/app/src/main/java/com/example/train_reservation/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.train_reservation.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.train_reservation.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
