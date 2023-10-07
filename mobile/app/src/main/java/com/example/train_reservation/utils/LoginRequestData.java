b54cb56f278d921f2a0fb832aac3d86d1cc300a1
hiru2k <mudannayakehiruni90@gmail.com>
2023-10-07 07:52:33 +0530

    created login requsted data

---
+++ mobile/app/src/main/java/com/example/train_reservation/utils/LoginRequestData.java
@@ -0,0 +1,15 @@
+package com.example.train_reservation.utils;
+
+public class LoginRequestData {
+
+    private String Username;
+    private String Password;
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
