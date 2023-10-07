8111cdf19e1e5662720f6ae1f86780b29f8412f9
hiru2k <mudannayakehiruni90@gmail.com>
2023-10-07 17:41:20 +0530

    added login response

--- mobile/app/src/main/java/com/example/train_reservation/utils/LoginResponse.java
+++ mobile/app/src/main/java/com/example/train_reservation/utils/LoginResponse.java
@@ -1,2 +1,17 @@
-package com.example.train_reservation.utils;public class LoginResponse {
+package com.example.train_reservation.utils;
+
+import com.example.train_reservation.models.User;
+
+public class LoginResponse {
+
+    private boolean success;
+    private String message;
+    private String token;
+    private User user;
+
+
+
+    public User getUser() {
+        return user;
+    }
 }
