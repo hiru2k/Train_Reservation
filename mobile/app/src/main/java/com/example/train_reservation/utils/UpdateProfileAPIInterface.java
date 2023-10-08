5a934bacd45efffeb5ed047a17529e0be7c0a227
hiru2k <mudannayakehiruni90@gmail.com>
2023-10-08 16:26:06 +0530

    completed update func without pswrd

---
+++ mobile/app/src/main/java/com/example/train_reservation/utils/UpdateProfileAPIInterface.java
@@ -0,0 +1,15 @@
+package com.example.train_reservation.utils;
+
+import com.example.train_reservation.models.User;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface UpdateProfileAPIInterface {
+    @PUT("profile/{nic}")
+    Call<Void> updateUserProfile(@Path("nic") String nic, @Body User user);
+}
+
+
