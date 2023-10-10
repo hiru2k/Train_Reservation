85ea8a840c361d2d4fcbbd5af384c5d5914c21fb
hiru2k <mudannayakehiruni90@gmail.com>
2023-10-10 09:41:03 +0530

    changed error msg order

--- web/src/components/mainpages/admin/CreateUser.jsx
+++ web/src/components/mainpages/admin/CreateUser.jsx
@@ -1,7 +1,13 @@
+/*
+ * Filename: CreateUser.jsx
+ * Description: Contains the UI and functionality for creating a new backoffice user+travel agent by admin
+ * Author: Hiruni Mudannayake
+ */
 import React from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import axiosInstance from "../../../DefaultHeader";
+import "./createUser.css";
 
 function UserRegistration() {
   const formik = useFormik({
@@ -15,9 +21,7 @@ function UserRegistration() {
     },
     validationSchema: Yup.object({
       Username: Yup.string().required("Username is required"),
-      NIC: Yup.string()
-        .required("NIC is required")
-        .matches(/^\d{12}$/, "NIC must be 12 digits"),
+      NIC: Yup.string().required("NIC is required"),
       Role: Yup.string().required("Role is required"),
       Email: Yup.string()
         .email("Invalid Email address")
@@ -55,64 +59,88 @@ function UserRegistration() {
   };
 
   return (
-    <div>
-      <h2>Create User Profile</h2>
-      <form onSubmit={formik.handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input type="text" {...formik.getFieldProps("Username")} />
-          {formik.touched.Username && formik.errors.Username ? (
-            <div className="error">{formik.errors.Username}</div>
-          ) : null}
+    <createUser>
+      <div className="background">
+        <ul>
+          <li></li>
+        </ul>
+        <div className="form">
+          <h2 className="formTitle">Create backoffice user profile</h2>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="errorAndInput">
+              {formik.touched.Username && formik.errors.Username ? (
+                <div className="error">{formik.errors.Username}</div>
+              ) : null}
+
+              <div className="formfields">
+                <label>Username:</label>
+                <input type="text" {...formik.getFieldProps("Username")} />
+              </div>
+            </div>
+            <div className="errorAndInput">
+              {formik.touched.NIC && formik.errors.NIC ? (
+                <div className="error">{formik.errors.NIC}</div>
+              ) : null}
+
+              <div className="formfields">
+                <label>NIC :</label>
+                <input
+                  type="text"
+                  {...formik.getFieldProps("NIC")}
+                  onChange={handleNICChange}
+                />
+              </div>
+            </div>
+
+            <div className="errorAndInput">
+              {formik.touched.Password && formik.errors.Password ? (
+                <div className="error">{formik.errors.Password}</div>
+              ) : null}
+              <div className="formfields">
+                <label>Password:</label>
+                <input type="Password" {...formik.getFieldProps("Password")} />
+              </div>
+            </div>
+
+            <div className="errorAndInput">
+              {formik.touched.Role && formik.errors.Role ? (
+                <div className="error">{formik.errors.Role}</div>
+              ) : null}
+              <div className="formfields">
+                <label>Role:</label>
+                <select {...formik.getFieldProps("Role")}>
+                  <option value="">Select a Role</option>
+                  <option value="Back Officer">Back Officer</option>
+                  <option value="Travel Agent">Travel Agent</option>
+                </select>
+              </div>
+            </div>
+            <div className="errorAndInput">
+              {formik.touched.Email && formik.errors.Email ? (
+                <div className="error">{formik.errors.Email}</div>
+              ) : null}
+
+              <div className="formfields">
+                <label>Email:</label>
+                <input type="Email" {...formik.getFieldProps("Email")} />
+              </div>
+            </div>
+
+            <div className="errorAndInput">
+              {formik.touched.Phone && formik.errors.Phone ? (
+                <div className="error">{formik.errors.Phone}</div>
+              ) : null}
+              <div className="formfields">
+                <label>Phone:</label>
+                <input type="text" {...formik.getFieldProps("Phone")} />
+              </div>
+            </div>
+
+            <button type="submit">Register</button>
+          </form>
         </div>
-        <div>
-          <label>NIC :</label>
-          <input
-            type="text"
-            {...formik.getFieldProps("NIC")}
-            onChange={handleNICChange}
-          />
-          {formik.touched.NIC && formik.errors.NIC ? (
-            <div className="error">{formik.errors.NIC}</div>
-          ) : null}
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type="Password" {...formik.getFieldProps("Password")} />
-          {formik.touched.Password && formik.errors.Password ? (
-            <div className="error">{formik.errors.Password}</div>
-          ) : null}
-        </div>
-        <div>
-          <label>Role:</label>
-          <select {...formik.getFieldProps("Role")}>
-            <option value="">Select a Role</option>
-            <option value="Back Officer">Back Officer</option>
-            <option value="Travel Agent">Travel Agent</option>
-          </select>
-          {formik.touched.Role && formik.errors.Role ? (
-            <div className="error">{formik.errors.Role}</div>
-          ) : null}
-        </div>
-        <div>
-          <label>Email:</label>
-          <input type="Email" {...formik.getFieldProps("Email")} />
-          {formik.touched.Email && formik.errors.Email ? (
-            <div className="error">{formik.errors.Email}</div>
-          ) : null}
-        </div>
-        <div>
-          <label>Phone:</label>
-          <input type="text" {...formik.getFieldProps("Phone")} />
-          {formik.touched.Phone && formik.errors.Phone ? (
-            <div className="error">{formik.errors.Phone}</div>
-          ) : null}
-        </div>
-        <div>
-          <button type="submit">Register</button>
-        </div>
-      </form>
-    </div>
+      </div>
+    </createUser>
   );
 }
 
