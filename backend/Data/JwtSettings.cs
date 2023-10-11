d5340f1ff7c69ff2a955e10db7e532fd373cc5c5
Srinidee789 <srinideemethmalmunasinghe@gmail.com>
2023-10-12 00:08:01 +0530

    Commiting changes before checkout

---
+++ backend/Data/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Data;
+
+
+
+public class JwtSettings
+{
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+    public string Key { get; set; } = null!;
+}
