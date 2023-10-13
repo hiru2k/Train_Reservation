2cd470052c3b054eee24c70d80204ef6c07bbd93
sathinka <sathinkawijesingha123@gmail.com>
2023-10-13 21:05:16 +0530

    remove duplicate code

--- backend/Services/TrainServices.cs
+++ backend/Services/TrainServices.cs
@@ -40,31 +40,7 @@ namespace backend.Services
             await _trainCollection.DeleteOneAsync(x=> x.Id == id);
 
 
-        private readonly IMongoCollection<Train> _trainCollection;
-
-        public TrainServices(IMongoDatabase database)
-        {
-
-            _trainCollection = database.GetCollection<Train>("Trains");
-        }
-        //get all trains
-        public async Task<List<Train>> GetAsync() => await _trainCollection.Find(_ => true).ToListAsync();
-
-        //get train by id
-        public async Task<Train> GetAsync(string id)=>
-            await _trainCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
-
-        //add new train
-        public async Task CreateAsync(Train newTrain) =>
-            await _trainCollection.InsertOneAsync(newTrain);
-
-        //update train
-        public async Task UpdateAsync(string id, Train updateTrain) =>
-            await _trainCollection.ReplaceOneAsync(x => x.Id == id, updateTrain);
-
-        //delete train
-        public async Task RemoveAsync(string id)=>
-            await _trainCollection.DeleteOneAsync(x=> x.Id == id);
+       
 
 
     }
